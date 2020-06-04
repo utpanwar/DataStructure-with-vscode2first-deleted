f09ceaa1b6cb98dbfce2f6bf29f38a67fe51528a
Utkarsh Panwar <utkarshpanwar212@gmail.com>
2020-06-04 19:51:41 +0530

    java branch

---
+++ Graph/Graph.java
@@ -0,0 +1,13 @@
+package Graph;
+
+import java.util.Scanner;
+
+public class Graph {
+    
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int m=s.nextInt();
+        System.out.println("hi");
+    }
+}
